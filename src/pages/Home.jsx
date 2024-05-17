ecdacb5f71f08f683adaac5b88db5977a8c8cc3d
nicole-santana <nicole.teles.santana@gmail.com>
2024-05-17 16:50:03 -0300

    criação dos primeiros estilos

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { HomeContainer } from './Home.styles'
+import Main from './Main'
+export default function Home() {
+  return (
+    <HomeContainer>
+      <Main></Main>
+    </HomeContainer>
+    
+  )
+}
---
+++ src/pages/Main.jsx
@@ -0,0 +1,11 @@
+export default function Main() {
+  return (
+    <div>
+        <img src="" alt="" />
+        <h1>Nicole De Sá</h1>
+        <p>São Paulo, Brasil</p>
+
+        <p>"Front-end developer"</p>
+    </div>
+  )
+}
